4da66509c8656e2ea09762c6db92a4e253300a73
kaanhascan <kaan.hascan@std.yeditepe.edu.tr>
2024-01-10 20:57:40 +0300

    Adding the order microservice file

---
+++ ordersvc/ordersvc/src/test/java/com/project/ordersvc/OrdersvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ordersvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
