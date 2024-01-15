4a314f5458ae9b8336f9d093e2bea7318676567d
kaanhascan <kaan.hascan@std.yeditepe.edu.tr>
2024-01-16 02:56:56 +0300

    Creating discovery service

---
+++ discoverysvc/src/main/java/com/project/Java/discoverysvc/DiscoverysvcApplication.java
@@ -0,0 +1,11 @@
+package com.project.Java.discoverysvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoverysvcApplication {
+    public static void main(String[] args) {SpringApplication.run(DiscoverysvcApplication.class, args);}
+}
